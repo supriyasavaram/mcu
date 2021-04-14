af52780cd8c7adced4dd43ef3429f72c3ed28727
Brandon Ongtingco <bmo4aa@virginia.edu>
2021-04-13 22:28:37 -0400

    Adjustments from Quyen's code

--- mcu_site/admin.py
+++ mcu_site/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import MovieDetails
-from .models import AccountDetails
+from .models import Person, Actor, Director, Movie, CharacterPlayed, Review, Profile, MovieDetails, AccountDetails
 
-# Register your models here.
+admin.site.register(Person)
+admin.site.register(Actor)
+admin.site.register(Director)
+
+admin.site.register(Movie)
+admin.site.register(CharacterPlayed)
+admin.site.register(Review)
 admin.site.register(MovieDetails)
 admin.site.register(AccountDetails)
