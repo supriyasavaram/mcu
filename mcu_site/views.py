2ba2f6fe333e151937a2b458e4370d6ae348f1a8
Quyen Huynh <qh8ar@virginia.edu>
2021-04-25 01:25:58 -0400

    user registration

--- mcu_site/views.py
+++ mcu_site/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from .models import Movie
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from .forms import CreateUserForm
 
 def index(request):
     return render(request, 'index.html')
@@ -24,7 +31,21 @@ def profile(request):
     return render(request, 'profile.html')
 
 def register(request):
-    return render(request, 'register.html')
+    context = {'error': ''}
+    if request.user.is_authenticated:
+        return redirect('index')
+    if request.method == "POST" and "register" in request.POST:
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            form.save()
+            # messages.success(request, "Account created!")
+            user = authenticate(request, username=username, password=password)
+            context = {'error' : 'created account'}
+        else:
+            context = {'form': form}
+    return render(request, 'register.html', context)
 
 def login(request):
     return render(request, 'login.html')
