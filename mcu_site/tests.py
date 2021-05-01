9723fce53c5ba412ff34758f2803ab9b9a952837
tnigro <59937017+tnigro@users.noreply.github.com>
2021-05-01 19:00:00 -0400

    Adding tests for Web app itself

--- mcu_site/tests.py
+++ mcu_site/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import *
+from .forms import *
 
-# Create your tests here.
+class ReviewTest(TestCase):
+    def test_standardReview(self):
+        form = CreateReviewForm(data = {
+            'title': 'Iron Man',
+            'stars': '4',
+            'review_text': 'Iron man good',
+            'author': 'Me',
+        })
+        self.assertTrue(form.is_valid())
+    def test_wrongReview(self):
+        form = CreateReviewForm(data = {
+            'title': 'Iron Man',
+            'stars': '-1',
+            'review_text': 'Iron man good',
+            'author': 'Me',
+        })
+        self.assertFalse(form.is_valid())
+    def test_standardReview(self):
+        form = CreateReviewForm(data = {
+            'title': 'Guardians of the Galaxy',
+            'stars': '0',
+            'review_text': 'nostalgia go brrr',
+            'author': 'Me',
+        })
+        self.assertTrue(form.is_valid())
+    def test_blankReview(self):
+        form = CreateReviewForm(data = {
+            'title': '',
+            'stars': '4',
+            'review_text': 'Iron man good',
+            'author': 'Me',
+        })
+        self.assertFalse(form.is_valid())
+class UserTest(TestCase):
+    def test_standardUser(self):
+        form = UserCreationForm(data = {
+            'first_name': 'Tim',
+            'last_name': 'Nigro',
+            'username': 'tnigro',
+            'email': 'tjn5hb@virginia.edu',
+            'password1': 'password',
+            'password2': 'lol',
+        })
+        self.assertTrue(form.is_valid())
+    def test_invalidUser(self):
+        form = UserCreationForm(data = {
+            'first_name': 'Tim',
+            'last_name': 'Nigro',
+            'username': '',
+            'email': 'tjn5hb@virginia.edu',
+            'password1': 'password',
+            'password2': 'lol',
+        })
+        self.assertFalse(form.is_valid())
